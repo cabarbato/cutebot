0a3253b261df18a69887c7526dc4174d8ccd7e1e
Colleen Barbato <colleen@barbato.io>
2023-03-11 13:59:21 -0500

    Add emotions

--- main.ts
+++ main.ts
@@ -5,6 +5,19 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         yValue = value
     }
+    if (("emote" as any) == ("0" as any)) {
+        basic.showIcon(IconNames.Heart)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+    } else if (("emote" as any) == ("1" as any)) {
+        basic.showIcon(IconNames.Sad)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+    } else if (("emote" as any) == ("2" as any)) {
+        basic.showIcon(IconNames.Angry)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+    } else if (("emote" as any) == ("3" as any)) {
+        basic.showIcon(IconNames.Asleep)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
+    }
 })
 let yValue = 0
 let xValue = 0
